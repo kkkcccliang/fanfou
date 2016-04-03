11d06ede4d10c9cf7148ed0caaeab5627113822d
liangjingzhan <fatzhan@163.com>
2016-04-03 11:12:01 +0800

    commit before adding backend

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,50 +17,8 @@ import {Home} from './home/home';
   providers: [ ...FORM_PROVIDERS ],
   directives: [ ...ROUTER_DIRECTIVES, RouterActive ],
   pipes: [],
-  styles: [`
-    nav ul {
-      display: inline;
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      width: 60px;
-    }
-    nav li {
-      display: inline;
-    }
-    nav li.active {
-      background-color: lightgray;
-    }
-  `],
-  template: `
-    <header>
-      <nav>
-        <h1>Hello {{ name }}</h1>
-        <ul>
-          <li router-active>
-            <a [routerLink]=" ['Index'] ">Index</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['Home'] ">Home</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['About'] ">About</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <footer>
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
-    </footer>
-  `
+  styles: [ require('./app.scss') ],
+  template: require('./app.html')
 })
 @RouteConfig([
   { path: '/', component: Home, name: 'Index' },
@@ -70,11 +28,13 @@ import {Home} from './home/home';
   { path: '/**', redirectTo: ['Index'] }
 ])
 export class App {
-  angularclassLogo = 'assets/img/angularclass-avatar.png';
-  name = 'Angular 2 Webpack Starter';
-  url = 'https://twitter.com/AngularClass';
+  angularClassLogo: string;
+  name: string;
+  url: string;
   constructor() {
-
+    this.angularClassLogo = 'assets/img/angularclass-avatar.png';
+    this.name = 'Angular 2 Webpack Starter';
+    this.url = 'https://twitter.com/AngularClass';
   }
 }
 
