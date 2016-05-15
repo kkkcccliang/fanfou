361eb1a90f3f6c356fab71bc5cd36fac2962bfc0
liangjingzhan <fatzhan@163.com>
2016-05-15 17:39:37 +0800

    feat: update restaurant

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -1,6 +1,6 @@
 /**
  * Created by liangjz on 4/3/16.
- * 
+ *
  * 餐馆
  */
 
@@ -10,7 +10,8 @@ var mongoose = require('mongoose');
 
 var restaurantSchema = new mongoose.Schema({
     isActive: Boolean, // 当前订饭的餐馆
-    name: String
+    name: String,
+    tel: String // 餐馆电话
 });
 
 module.exports = mongoose.model('restaurant', restaurantSchema);
