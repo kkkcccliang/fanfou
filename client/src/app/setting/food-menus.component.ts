ecf5cd05cc7256ce978e73466bd8bf856a9e6ca7
liangjingzhan <fatzhan@163.com>
2016-05-22 10:52:49 +0800

    update food-menus

--- client/src/app/setting/food-menus.component.ts
+++ client/src/app/setting/food-menus.component.ts
@@ -3,12 +3,16 @@
  */
 
 import {Component} from '@angular/core';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MdButton} from '@angular2-material/button';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 @Component({
-    selector: 'food-menus',
-    template: require('./food-menus.html')
+  selector: 'food-menus',
+  directives: [MD_INPUT_DIRECTIVES, MdButton, MD_LIST_DIRECTIVES],
+  template: require('./food-menus.html')
 })
 export class FoodMenus {
-    constructor() {
-    }
+  constructor() {
+  }
 }
